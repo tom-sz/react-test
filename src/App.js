58c5c6dba820a96761b5084199535c47a03ffa84
tom-sz <57638712+tom-sz@users.noreply.github.com>
2023-07-07 10:42:07 +0200

    Update App.js
    first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - 2023
         </a>
       </header>
     </div>
